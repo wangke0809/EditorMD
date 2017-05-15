8d3f2c1ff3bf7f920f2ff47fb4d820a1b5820440
DT27 <dragonet1943@gmail.com>
2017-05-15 16:42:47 +0800

    Chrome中，图片粘贴上传改为仅支持单张上传以修复无法粘贴问题

--- Plugin.php
+++ Plugin.php
@@ -219,55 +219,47 @@ class EditorMD_Plugin implements Typecho_Plugin_Interface
                             return;
                         }
 
-                        if ((itemLength == 1 && cbd.items[0].kind == 'file')
-                                || itemLength > 1
-                            ) {
-                            for (var i = 0; i < cbd.items.length; i++) {
-                                var item = cbd.items[i];
-
-                                if(item.kind == "file") {
-                                    var blob = item.getAsFile();
-                                    if (blob.size === 0) {
-                                        return;
-                                    }
-                                    var ext = 'jpg';
-                                    switch(blob.type) {
-                                        case 'image/jpeg':
-                                        case 'image/pjpeg':
-                                            ext = 'jpg';
-                                            break;
-                                        case 'image/png':
-                                            ext = 'png';
-                                            break;
-                                        case 'image/gif':
-                                            ext = 'gif';
-                                            break;
-                                    }
-                                    var formData = new FormData();
-                                    formData.append('blob', blob, Math.floor(new Date().getTime() / 1000) + '.' + ext);
-                                    var uploadingText = '![图片上传中(' + i + ')...]';
-                                    var uploadFailText = '![图片上传失败(' + i + ')]'
-                                    postEditormd.insertValue(uploadingText);
-                                    $.ajax({
-                                        method: 'post',
-                                        url: uploadURL.replace('CID', $('input[name="cid"]').val()),
-                                        data: formData,
-                                        contentType: false,
-                                        processData: false,
-                                        success: function(data) {
-                                            if (data[0]) {
-                                                postEditormd.setValue(postEditormd.getValue().replace(uploadingText, '![](' + data[0] + ')'));
-                                            } else {
-                                                postEditormd.setValue(postEditormd.getValue().replace(uploadingText, uploadFailText));
-                                            }
-                                        },
-                                        error: function() {
-                                            postEditormd.setValue(postEditormd.getValue().replace(uploadingText, uploadFailText));
-                                        }
-                                    });
-                                }
-
+                        if ((itemLength == 1 && cbd.items[0].kind == 'file')) {
+                            var item = cbd.items[0];
+                            var blob = item.getAsFile();
+                            if (blob.size === 0) {
+                                return;
                             }
+                            var ext = 'jpg';
+                            switch(blob.type) {
+                                case 'image/jpeg':
+                                case 'image/pjpeg':
+                                    ext = 'jpg';
+                                    break;
+                                case 'image/png':
+                                    ext = 'png';
+                                    break;
+                                case 'image/gif':
+                                    ext = 'gif';
+                                    break;
+                            }
+                            var formData = new FormData();
+                            formData.append('blob', blob, Math.floor(new Date().getTime() / 1000) + '.' + ext);
+                            var uploadingText = '![图片上传中(' + i + ')...]';
+                            var uploadFailText = '![图片上传失败(' + i + ')]'
+                            postEditormd.insertValue(uploadingText);
+                            $.ajax({
+                                method: 'post',
+                                url: uploadURL.replace('CID', $('input[name="cid"]').val()),
+                                data: formData,
+                                contentType: false,
+                                processData: false,
+                                success: function(data) {
+                                    if (data[0]) {
+                                        postEditormd.setValue(postEditormd.getValue().replace(uploadingText, '![](' + data[0] + ')'));
+                                    } else {
+                                        postEditormd.setValue(postEditormd.getValue().replace(uploadingText, uploadFailText));
+                                    }
+                                },
+                                error: function() {
+                                    postEditormd.setValue(postEditormd.getValue().replace(uploadingText, uploadFailText));
+                                }
+                            });
                             return false;
                         }
 
